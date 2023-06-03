fe7debbdbb700f6dd28a6a24c7c087cbd75599d3
modybick <haruki.mjp@gmail.com>
2023-06-03 17:57:44 +0900

    TODOレイアウト

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  background-color: rgb(171, 213, 230);
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 8px;
+}
+</style>
